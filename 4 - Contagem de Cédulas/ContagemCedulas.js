0918214f3ec5141c7588f602a35cf5aeb409392d
Felipe Vencato <felipevencato@gmail.com>
2021-03-20 21:32:37 -0300

    Solução para o desafio 4, Contagem de Cédulas

---
+++ 4 - Contagem de Cédulas/ContagemCedulas.js
@@ -0,0 +1,7 @@
+let valor = parseInt(gets())
+console.log(valor);
+[100, 50, 20, 10, 5, 2, 1].forEach(x => {
+  let qtd=Math.floor(valor / x)
+  valor -= x * qtd
+  console.log(qtd + " nota(s) de R$ " + x + ",00")
+})
